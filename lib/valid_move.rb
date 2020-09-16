068092ce3e18a56fd828460822726aa89fa20d28
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-16 02:07:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,5 @@
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    true
-  else
-    false
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
